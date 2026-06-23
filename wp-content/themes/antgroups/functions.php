f085cbb2cfa9459b32e489ccb1ad72a149717b6b
Farhan Kamarzaman <farhan@dzvisual.my>
2026-06-23 11:29:20 +0800

    Update site components and content

--- wp-content/themes/antgroups/functions.php
+++ wp-content/themes/antgroups/functions.php
@@ -1,11 +1,2 @@
 <?php
-function at_group_setup() {
-    // Adds support for a custom logo via the WordPress Customizer
-    add_theme_support('custom-logo', array(
-        'height'      => 100,
-        'width'       => 100,
-        'flex-height' => true,
-        'flex-width'  => true,
-    ));
-}
-add_action('after_setup_theme', 'at_group_setup');
+// Standalone site placeholder.
--- wp-content/themes/antgroups/header.php
+++ wp-content/themes/antgroups/header.php
@@ -1,12 +1,3 @@
-<?php
-if ( has_custom_logo() ) {
-    // Use WordPress Custom Logo if set
-    the_custom_logo();
-} else {
-    // Fallback logo from theme folder
-    $logo_url = get_template_directory_uri() . '/assets/images/logo.png';
-    echo '<a href="' . esc_url(home_url('/')) . '">';
-    echo '<img src="' . esc_url($logo_url) . '" alt="' . esc_attr(get_bloginfo('name')) . '">';
-    echo '</a>';
-}
-?>
+<a href="/">
+    <img src="assets/images/logo.png" alt="A&T Group logo">
+</a>
